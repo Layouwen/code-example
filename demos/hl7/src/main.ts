792aca26b300b64f8c6e7bd64407945b147f8c2f
Avan <layouwen@gmail.com>
2022-11-25 18:23:37 +0800

    chroe: 发送ATD_A01所有包

--- demos/hl7/src/main.ts
+++ demos/hl7/src/main.ts
@@ -3,28 +3,28 @@ import * as net from 'net'
 import * as fs from 'fs'
 import path from 'path'
 
-// const package1 = (str: string): Buffer => {
-//   const buf = Buffer.from(str)
-//   let newBuf = Buffer.concat([Buffer.from([0x0b]), buf, Buffer.from([0x1c, 0x0d])], buf.length + 3)
-//   return newBuf
-// }
+const package1 = (str: string): Buffer => {
+  const buf = Buffer.from(str)
+  let newBuf = Buffer.concat([Buffer.from([0x0b]), buf, Buffer.from([0x1c, 0x0d])], buf.length + 3)
+  return newBuf
+}
 
-// var client = new net.Socket()
-// client.connect(8009, '127.0.0.1', function () {
-//   const content = fs.readFileSync(path.resolve(__dirname, './resFakeData'), 'utf-8')
-//   client.write(package1(content))
-// })
+var client = new net.Socket()
+client.connect(8009, '127.0.0.1', function () {
+  const A01Path = path.resolve(__dirname, 'mock/ADT^A01^ADT_A01')
 
-// client.on('close', function () {
-//   console.log('Connection closed')
-// })
+  const dirs = fs.readdirSync(A01Path)
+  // const filename = dirs[1]
+  dirs.forEach(filename => {
+    const content = fs.readFileSync(path.resolve(A01Path, filename), 'utf-8')
+    const data = parse(content)
+    console.log('====================================')
+    console.log(data)
+    client.write(package1(content))
+    console.log('====================================')
+  })
+})
 
-const A01Path = path.resolve(__dirname, 'mock/ADT^A01^ADT_A01')
-
-const dirs = fs.readdirSync(A01Path)
-dirs.forEach(filename => {
-  const data = parse(fs.readFileSync(path.resolve(A01Path, filename), 'utf-8'))
-  console.log('====================================')
-  console.log(data)
-  console.log('====================================')
+client.on('close', function () {
+  console.log('Connection closed')
 })
