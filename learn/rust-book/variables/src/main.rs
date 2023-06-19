8927de017f37b8d200c62909066c376c8989f37d
Avan <layouwen@gmail.com>
2023-06-19 18:03:23 +0800

    chore: todo variables

---
+++ learn/rust-book/variables/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let mut x = 5;
+
+    println!("The value of x is: {x}");
+
+    x = 6;
+    println!("The value of x is: {x}");
+
+    let a = "Hello";
+    println!("The value of a is: {a}");
+
+    let a = a.len();
+    println!("The value of a is: {a}");
+}
