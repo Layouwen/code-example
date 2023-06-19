f359ffdc5094b66caa322ce87c29f41307346af7
Avan <layouwen@gmail.com>
2023-06-19 23:18:25 +0800

    feat: 数组类型

--- learn/rust-book/variables/src/main.rs
+++ learn/rust-book/variables/src/main.rs
@@ -1,14 +1,22 @@
+use std::io;
+
 fn main() {
-    let mut x = 5;
+    let a = [1, 2, 3, 4, 5];
 
-    println!("The value of x is: {x}");
+    println!("Please enter an array index.");
 
-    x = 6;
-    println!("The value of x is: {x}");
+    let mut index = String::new();
 
-    let a = "Hello";
-    println!("The value of a is: {a}");
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
 
-    let a = a.len();
-    println!("The value of a is: {a}");
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!("The value of the element at index {index} is: {element}");
 }
